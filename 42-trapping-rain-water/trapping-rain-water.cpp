9bfc7fa6546f81bcc059a3fca03a676edfa35eb5
Jeet Bharti <144922291+JeetBharti@users.noreply.github.com>
2025-07-06 16:17:21 +0530

    Time: 4 ms (11.38%) | Memory: 27.3 MB (31.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        stack<int>st;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && height[i]>height[st.top()]){
-                int curr = height[st.top()];
-                st.pop();
-                if(st.empty())
-                break;
-                int width = i-st.top()-1;
-                ans += (min(height[st.top()],height[i])-curr)*width;
-            }
-            st.push(i);
+        vector<int>left(n);
+        vector<int>right(n);
+        left[0]=height[0];
+        right[n-1]=height[n-1];
+
+        for(int i=1;i<n;i++){
+            left[i]=max(left[i-1],height[i]);
         }
-        return ans;
-                
+
+        for(int j=n-2;j>=0;j--){
+            right[j]=max(right[j+1],height[j]);
+        }
+
+        // // for(int i=0;i<n;i++){
+        // //     cout<<right[i]<<" ";
+        // }
+        int sum=0;
+        for(int a=0;a<n;a++){
+            int result=min(left[a],right[a]) - height[a];
+            sum=sum+result;
+        }
+        return sum; 
     }
 };
