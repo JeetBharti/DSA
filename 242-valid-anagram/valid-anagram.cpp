03a77c3fb4e3d6ab19a46ba1438429534ef40afc
Jeet Bharti <144922291+JeetBharti@users.noreply.github.com>
2025-10-08 15:22:27 +0530

    Time: 7 ms (20.05%) | Memory: 9.7 MB (74.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>mp;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
 
-        for(char ch : s){
-            mp[ch]++;
-        }
-
-        for(char ch : t){
-            mp[ch]--;
-        }
-
-        for(auto &i: mp){
-            if(i.second!=0){
-                return false;
-            }
-        }
-        return true;
-        
+        return s==t;
         
     }
 };
